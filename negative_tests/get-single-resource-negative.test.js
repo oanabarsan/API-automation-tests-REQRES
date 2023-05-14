35b79a83fbd2e8d348ff59e51246d8ed146a4603
oana maria barsan <oanabarsan@yahoo.com>
2023-05-14 12:07:46 +0300

    create negative test for get single resource

--- negative_tests/get-single-resource-negative.test.js
+++ negative_tests/get-single-resource-negative.test.js
@@ -7,7 +7,7 @@ describe("Try to get resource user negative test with an URL that does not match
   it("Get resource user negative test with unmatched URL", async () => {
     await spec()
     .get(`${baseUrl}/api/unknown/233sss`)
-    .expectStatus(404);
+    .expectStatus(404); //expected status "Pass"
   });
 
 });
@@ -18,7 +18,7 @@ describe("Try to get single resource negative test with body contains that does 
   it("Get single user resource test with unmatched body contains", async () => {
     await spec()
     .get(`${baseUrl}/api/unknown/2`)
-    .expectBodyContains("fuchsia blue");
+    .expectBodyContains("fuchsia blue"); //expected status "Fail"
   });
 
 });
@@ -28,7 +28,7 @@ describe("Try to get resource user negative test with shorter response time", ()
   it("Get single user negative test for shorter response time", async () => {
     await spec()
     .get(`${baseUrl}/api/unknown/2`)
-    .expectResponseTime(2);
+    .expectResponseTime(2); //expected status "Fail"
   });
 
 });
@@ -40,7 +40,7 @@ describe("Try to get resource user negative test with different Json schema", ()
   it("Get single resource negative test with different json schema", async () => {
     await spec()
     .get(`${baseUrl}/api/unknown/2`)
-    .expectJsonSchema(requestSchema);
+    .expectJsonSchema(requestSchema); //expected status "Fail"
   });
 
 });
