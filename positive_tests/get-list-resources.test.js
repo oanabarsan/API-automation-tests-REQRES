0c45d0a92bb5fda070e451694ae93aed502b34bd
oana maria barsan <oanabarsan@yahoo.com>
2023-05-14 17:53:23 +0300

    create positive test for get single and list resources

--- positive_tests/get-list-resources.test.js
+++ positive_tests/get-list-resources.test.js
@@ -4,7 +4,7 @@ const baseUrl = "https://reqres.in";
 
 const getResourceSchema = require("../data/response/get-resources-list-schema.json");
 
-describe("Get list resources page 1 and 2 endpoint test suite", () => {
+describe("Get single and list resources page 1 and 2 endpoint test suite", () => {
   before(() => {
     request.setDefaultTimeout(5000);
   });
@@ -25,5 +25,15 @@ describe("Get list resources page 1 and 2 endpoint test suite", () => {
       .expectResponseTime(3000)
       .expectBodyContains("blue iris")
       .expectJsonSchema(getResourcesSchema);
+  });
+
+  it("Get single resource test", async () => {
+    await spec()
+      .get(`${baseUrl}/api/unknown/2`)
+      .expectStatus(200)
+      .expectResponseTime(3000)
+      .expectBodyContains("fuchsia rose")
+      .expectJsonSchema(getResourceSchema)
+      .withQueryParams('id', '2');
   });
 });
