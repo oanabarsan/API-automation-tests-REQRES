e8161a775c55d22c4bf60a3e8abff91a04d08f01
oana maria barsan <oanabarsan@yahoo.com>
2023-05-14 12:35:31 +0300

    create positive test for get single resource

--- positive_tests/get-single-resource.test.js
+++ positive_tests/get-single-resource.test.js
@@ -4,12 +4,12 @@ const baseUrl = "https://reqres.in";
 
 const getResourceSchema = require("../data/response/get-single-resource-schema.json");
 
-describe("Get single resources endpoint test suite", () => {
+describe("Get single resource endpoint test suite", () => {
   before(() => {
     request.setDefaultTimeout(5000);
   });
 
-  it("Get single resources test", async () => {
+  it("Get single resource test", async () => {
     await spec()
       .get(`${baseUrl}/api/unknown/2`)
       .expectStatus(200)
